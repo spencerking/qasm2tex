e8db0c1c8edffd916aadc8c7821a431f8d021d77
Spencer King <spencer@spencertking.com>
2021-05-21 16:27:28 -0500

    Start parsing files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "/Users/spencerking/Documents/qasm2tex/examples/test1.qasm";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    for (index, line) in reader.lines().enumerate() {
+	let line = line.unwrap();
+
+	if line.is_empty() {
+	    continue;
+	}
+	
+	if line.chars().next().unwrap() == '#' {
+	    continue;
+	}
+	
+	println!("{}", line);
+    }
 }
