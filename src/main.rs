10f9e8892bbfe613beb2e51a62ffc1510c8e3154
Spencer King <spencer@spencertking.com>
2021-05-23 15:21:08 -0500

    Parsed a simple file into qubits and gates

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,53 @@ fn main() {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
+    let mut qubits: Vec<String> = Vec::new();
+    let mut gates: Vec<(String, String)> = Vec::new();
+    
     for (index, line) in reader.lines().enumerate() {
-	let line = line.unwrap();
-
+	let mut line = line.unwrap();
+	
+	// Ignore empty lines
 	if line.is_empty() {
 	    continue;
 	}
+
+	// Remove leading and trailing whitespace
+	line = line.trim().to_string();
 	
+	// Ignore comment lines
 	if line.chars().next().unwrap() == '#' {
 	    continue;
 	}
+
+	// Strip comments in line
+	let comment_pos = line.chars().position(|c| c == '#');
+	if comment_pos != None {
+	    line.replace_range(comment_pos.unwrap().., "");
+	}
+
+	let s: Vec<&str> = line.split_whitespace().collect(); //split('\t').collect();
+	//s = s.iter().map(|x| x.trim()).collect();
+
+	// qubits
+	if s[0].to_string() == "qubit" {
+	    //println!("qubit");
+	    qubits.push(s[1].to_string());
+	    //continue;
+	} else {
+	    gates.push((s[0].to_string(), s[1].to_string()));
+	    //continue;
+	}
 	
-	println!("{}", line);
+	//println!("{}", line);
+	//println!("\n");
+    }
+    
+    for qubit in qubits.iter() {
+	println!("{}", qubit);
+    }
+
+    for gate in gates.iter() {
+	println!("{:#?}", gate);
     }
 }
